3a66f152d157bdae2dd442caa2b4cf5b5ce02dc3
devgiljong <calf-love@naver.com>
2023-11-27 01:01:47 +0900

    :sparkles: 타입스크립트 설정 추가 및 컴파일

---
+++ dist/index.cjs
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+};
