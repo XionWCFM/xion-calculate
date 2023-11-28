72dea64167bd37171892802f46f76262488d58a2
devgiljong <calf-love@naver.com>
2023-11-28 17:22:03 +0900

    :sparkles: 플러그인 추가

--- build.js
+++ build.js
@@ -1,27 +1,33 @@
-import esbuild from 'esbuild';
+import esbuild from "esbuild";
+import { nodeExternalsPlugin } from "esbuild-node-externals";
 // 공통으로 사용할 옵션들
 // https://esbuild.github.io/api/#build 에서 다양한 옵션들을 확인할 수 있다.
 const baseConfig = {
-  entryPoints: ['src/index.ts'], // 컴파일할 파일
-  outdir: 'dist', // 컴파일된 파일이 저장될 경로
-  bundle: true, // 번들링 여부
-  sourcemap: true, // 소스맵 생성 여부
+  entryPoints: ["./src/index.ts"],
+  outfile: "dist/index.js",
+  bundle: true,
+  minify: true,
+  treeShaking: true,
+  platform: "node",
+  format: "cjs",
+  target: "node14",
+  plugins: [nodeExternalsPlugin()],
 };
 Promise.all([
   // 한 번은 cjs
   esbuild.build({
     ...baseConfig,
-    format: 'cjs',
+    format: "cjs",
     outExtension: {
-      '.js': '.cjs',
+      ".js": ".cjs",
     },
   }),
   // 한 번은 esm
   esbuild.build({
     ...baseConfig,
-    format: 'esm',
+    format: "esm",
   }),
 ]).catch(() => {
-  console.log('Build failed');
+  console.log("Build failed");
   process.exit(1);
 });
