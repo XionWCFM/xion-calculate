ef9bd20d930e63b44b6fb5982473438a397da4f6
devgiljong <calf-love@naver.com>
2023-11-28 17:32:48 +0900

    :sparkles: Add

--- src/use-debounce.ts
+++ src/use-debounce.ts
@@ -1,16 +1,18 @@
-import { useRef } from "react";
+import { useState } from "react";
 
 export const useDebounce = <ParamType extends unknown[]>(
   callback: (...params: ParamType) => void,
   time: number,
 ) => {
-  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
-  return (...params: ParamType) => {
-    if (timer.current) clearTimeout(timer.current);
+  const [hi,setHi] = useState(false)
+  // const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
+  // return (...params: ParamType) => {
+  //   if (timer.current) clearTimeout(timer.current);
 
-    timer.current = setTimeout(() => {
-      callback(...params);
-      timer.current = null;
-    }, time);
-  };
+  //   timer.current = setTimeout(() => {
+  //     callback(...params);
+  //     timer.current = null;
+  //   }, time);
+  // };
+  return [hi,setHi]
 };
