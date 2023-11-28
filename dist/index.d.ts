e1063402a712a6ce56cb64f708344b5bcb4d9ab6
devgiljong <calf-love@naver.com>
2023-11-28 17:02:42 +0900

    :sparkles: 결과물 확인

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
-export declare const helloWorld: (param: string | number) => string | number;
-export declare const amazingAlert: (param: string | number) => void;
+import { useDebounce } from "./use-debounce";
+export { useDebounce };
